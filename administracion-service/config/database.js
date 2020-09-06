5351d263c7290bb8e78d0952c2fd0e54a7e54b07
johnalem <51495163+johnalem@users.noreply.github.com>
2020-09-06 13:43:40 -0500

    commit conexion database

--- administracion-service/config/database.js
+++ administracion-service/config/database.js
@@ -8,10 +8,10 @@ module.exports = {
     poolIncrement: 0
   },
 
-  hrPool: {
-    user: process.env.ORA_USER_MONITOR = "USURIPSSDF",
-    password: process.env.ORA_PASSWORD_MONITOR = "Desaripss2019",
-    connectString: process.env.ORA_CONNECTIONSTRING_MONITOR = "srvvdesabd-scan.colsanitas.com:1521/DRIPSSDF",
+   hrPool: {
+    user: process.env.ORA_USER_MONITOR = "RIPSS",
+    password: process.env.ORA_PASSWORD_MONITOR = "yE91ijGeTL",
+    connectString: process.env.ORA_CONNECTIONSTRING_MONITOR = "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=204.110.52.42)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)))",
     poolMin: 10,
     poolMax: 10,
     poolIncrement: 0
